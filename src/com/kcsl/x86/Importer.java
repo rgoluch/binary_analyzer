fe6a3bece371283e77f50c2dd2bf881b82f799fa
rgoluch <rgoluch@iastate.edu>
2020-02-24 11:44:46 -0600

    working on adding path counting

--- src/com/kcsl/x86/Importer.java
+++ src/com/kcsl/x86/Importer.java
@@ -1,7 +1,11 @@
 package com.kcsl.x86;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -14,10 +18,21 @@ import com.ensoftcorp.atlas.core.db.graph.Node;
 import com.ensoftcorp.atlas.core.query.Q;
 import com.ensoftcorp.atlas.core.query.Query;
 import com.ensoftcorp.atlas.core.xcsg.XCSG;
-import com.ensoftcorp.atlas.core.xcsg.XCSG.*;
+//import com.se421.paths.algorithms.PathCounter.CountingResult;
+import com.se421.paths.algorithms.counting.DFSPathCounter;
+import com.se421.paths.algorithms.counting.MultiplicitiesPathCounter;
+import com.kcsl.paths.*;
+import com.kcsl.paths.counting.TopDownDFMultiplicitiesPathCounter;
+import com.kcsl.paths.algorithms.PathCounter.*;
 
 public class Importer {
 
+	protected static final String resultsPath = "/Users/RyanGoluch/Desktop/My_Results.csv";
+	
+	protected static final String headers = "Function Name,numPaths (NonLinear),additions (NonLinear),numPaths (Linear),additions (Linear)\n";
+	
+	private static ArrayList<Node> function_nodes = new ArrayList<Node>();
+	
 	public static void main() throws FileNotFoundException {
 		// HashMap of Node for further access
 		Map<String,Node> nodeMap = new HashMap<String,Node>();
@@ -30,10 +45,12 @@ public class Importer {
 		for(File dot : dirList) {
 			if (dot.exists()) {
 				count++;
-				System.out.println("Count: " +count+" Function: "+dot.getName());
+//				System.out.println("Count: " +count+" Function: "+dot.getName());
 				Node functionName = Graph.U.createNode();
 				functionName.putAttr(XCSG.name, "sym_"+dot.getName().replace(".dot", ""));
 				functionName.tag(XCSG.Function);
+				functionName.tag("binary_function");
+				function_nodes.add(functionName);
 				Scanner s = new Scanner(dot);
 
 				while(s.hasNextLine()) {
@@ -55,7 +72,7 @@ public class Importer {
 					}
 					else if(data.contains("->")) {
 						data = data.replaceAll("\\s+", "");
-						System.out.println(data);
+//						System.out.println(data);
 						//Extract the addresses of the from and to nodes in DOT file
 						String from = data.split("->")[0];
 						from = from.replaceAll("\"", "");
@@ -112,7 +129,62 @@ public class Importer {
 	}
 	
 	public static Q my_cfg(Q f) {
-		return f.contained().induce(Query.universe().edges(XCSG.ControlFlow_Edge));
+		return f.contained().nodes(XCSG.ControlFlow_Node).induce(Query.universe().edges(XCSG.ControlFlow_Edge));
 	}
-
+	
+	public static void export_counts() throws IOException {
+				
+//		try {
+			File results = new File(resultsPath);
+			BufferedWriter resultsWriter = new BufferedWriter(new FileWriter(results));
+			resultsWriter.write(headers);
+			TopDownDFMultiplicitiesPathCounter nonLinearCounter = new TopDownDFMultiplicitiesPathCounter();
+//			MultiplicitiesPathCounter linearCounter = new MultiplicitiesPathCounter();
+			
+			// We will now generate the results for all the functions in the graph database.
+			// It is assumed that you have XINU mapped into Atlas before you run this code.
+//			Q app = SetDefinitions.app();
+//		 	Q functions = Common.universe().nodes(XCSG.Function).nodes("my_function");
+		 			//app.nodes(XCSG.Function).nodesTaggedWithAll("my_function");
+			for(Node function : function_nodes) {
+				
+				if(function.getAttr(XCSG.name).toString().equals("sym_test_libCtype") || function.getAttr(XCSG.name).toString().equals("sym_fputs")) {
+					continue;
+				}
+				
+				Q temp = my_function(function.getAttr(XCSG.name).toString());
+				Q c = my_cfg(temp);
+				System.out.println(function.getAttr(XCSG.name));
+				CountingResult nonLinear = nonLinearCounter.countPaths(c);
+//				CountingResult linear = linearCounter.countPaths(c);
+				
+				// function name
+				resultsWriter.write(function.getAttr(XCSG.name) + ",");
+				
+				// number of paths according to nonLinear algorithm
+				resultsWriter.write(nonLinear.getPaths() + ",");
+				
+				// number of additions by nonLinear algorithm
+				resultsWriter.write(nonLinear.getAdditions() + ",");
+				
+//				// number of paths according to linear algorithm
+//				resultsWriter.write(linear.getPaths() + ",");
+//				
+//				// number of additions by linear algorithm
+//				resultsWriter.write(linear.getAdditions() + "\n");
+//				
+				// flushing the buffer
+				resultsWriter.flush();
+			}
+			
+			resultsWriter.close();
+//			
+//		} catch(FileNotFoundException e) {
+//			Log.error(e.getMessage(), e);
+//		} catch(IOException e) {
+//			Log.error(e.getMessage(), e);
+//		} finally {
+//			System.out.println("Executed export paths");
+//		}
+	}
 }
